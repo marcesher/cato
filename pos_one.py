d270477c665e56bffdc07b59fe60e6cc338f3b05
bill shelton <bill@if.io>
2013-03-03 15:52:05 -0500

    Making line numbers dissimilar

--- pos_one.py
+++ pos_one.py
@@ -9,6 +9,19 @@ import sys
 class Massage:
 
     def __init__(self):
+        pass
+
+
+    def __unicode__(self):
+        return "I want from you the massagem you idiot!"
+
+
+
+ 
+
+
+class Minkey:    
+    def __init__(self):
         self.admin = 'root'
         self.email = 'admin@gov.gov'
         self.password = 'P@ssW0rd'
